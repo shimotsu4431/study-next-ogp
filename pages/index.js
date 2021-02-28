4848e202ba3c785c0bd6debfe0336c1d38ae01c2
shimotsu4431 <shimotsu13@gmail.com>
2021-02-28 23:36:49 +0900

    remove

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,8 @@
-import useSWR from 'swr'
-import axios from "axios";
 import Link from "next/link"
-import getConfig from 'next/config'
-
 import CommonMeta from '../components/CommonMeta'
 import styles from '../styles/Home.module.css'
 
-const { publicRuntimeConfig } = getConfig()
-const { API_URL } = publicRuntimeConfig
-
 export default function Home() {
-  const fetcher = (url)=> axios(url).then(res => res.data)
-  const { data, error } = useSWR(`${API_URL}/photos`, fetcher)
-
-  if (error) return <div>failed to load</div>
 
   return (
     <div className={styles.container}>
