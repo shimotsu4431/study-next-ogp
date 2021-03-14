113387f716c9189676cc82fa4465db00b966cb4d
shimotsu4431 <shimotsu13@gmail.com>
2021-03-14 18:03:33 +0900

    useSWR

--- pages/photos-ssr/[photo_id]/index.js
+++ pages/photos-ssr/[photo_id]/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import useSWR from 'swr'
 import Head from 'next/head'
 import getConfig from 'next/config'
 import styles from '../../../styles/Home.module.css'
@@ -9,34 +10,32 @@ export const fetcher = (url)=> axios(url).then(res => res.data)
 
 export async function getServerSideProps({ query }) {
   const data = await fetcher(`${API_URL}/photos/${query.photo_id}`)
-
-  return {
-    props: {
-      photoId: parseInt(query.photo_id),
-      src: data.url,
-      ogp: data.thumbnailUrl,
-      title: data.title
-    },
-  }
+  return { props: { data, query } }
 }
 
-export default function Photo({ photoId, ogp, src, title }) {
+export default function Photo(props) {
+  const initialData = props.data
+  const { data } = useSWR(`${API_URL}/todos`, fetcher, {
+    initialData,
+    revalidateOnFocus : false
+  })
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>【SSR】Photo_id: {photoId}</title>
-        <meta property="og:title" content={`【SSR】photo_id: ${photoId}`} />
-        <meta property="og:description" content={`${title}`} />
-        <meta property="og:image" content={ogp} />
+        <title>【SSR】Photo_id: {props.query.photo_id}</title>
+        <meta property="og:title" content={`【SSR】photo_id: ${props.query.photo_id}`} />
+        <meta property="og:description" content={`${data.title}`} />
+        <meta property="og:image" content={data.thumbnailUrl} />
         <meta name="twitter:card" content="summary"/>
       </Head>
 
       <main className={styles.main}>
         <h1>Photos - SSR</h1>
         <h2>
-          Photo_Id: {photoId}
+          Photo_Id: {props.query.photo_id}
         </h2>
-        <img src={src} alt={title} />
+        <img src={data.url} alt={data.title} />
       </main>
     </div>
   )
