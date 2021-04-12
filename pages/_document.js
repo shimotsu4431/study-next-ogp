650df41e6d9c72663d794b977e7ecb2e25a81209
shimotsu4431 <shimotsu13@gmail.com>
2021-04-12 20:21:50 +0900

    fix _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,10 @@ class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
+          <title>My App</title>
+          <meta property="og:title" content="My App" />
+          <meta property="og:description" content="This is MY App." />
+          <meta name="twitter:card" content="summary"/>
           <meta name="format-detection" content="telephone=no" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <link rel="icon" href="/img/favicon.ico" />
